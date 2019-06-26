fe0215acf885074c61062e00bc89ac72186c9dab
ims <optimizes@126.com>
2019-06-27 07:03:37 +0800

    add templete project

---
+++ templete/code/project/build.sh
@@ -0,0 +1,4 @@
+
+rm -rf build/*
+cd build
+cmake ..
---
+++ templete/code/project/main.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: ims
+    > Created Time: 2019年06月26日 星期三 21时56分22秒
+ ************************************************************************/
+
+#include<iostream>
+#include<i>
+using namespace std;
+
+int main()
+{
+	int
+	char *p = "hello";
+	cout<<"hello"<<endl;
+	return 0;
+}
