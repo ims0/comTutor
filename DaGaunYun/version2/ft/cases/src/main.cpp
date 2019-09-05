95889b64452173cb40a13a86e363691937ed4bd1
ims phone <you@example.com>
2019-09-05 21:51:41 +0800

    add

--- DaGaunYun/version2/ft/cases/src/main.cpp
+++ DaGaunYun/version2/ft/cases/src/main.cpp
@@ -5,10 +5,12 @@
  ************************************************************************/
 
 #include<gtest/gtest.h>
+#include<test_case.h>
 
 int main(int argc, char **argv)
 {
     testing::GTEST_FLAG(break_on_failure) = 0;
     testing::InitGoogleTest(&argc, argv) ;
+    testing::AddGlobalTestEnvironment(new FooEnvironment()) ;
     return RUN_ALL_TESTS();
 }
