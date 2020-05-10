afe1b66fa9c5a072ba471e873aded23b79711231
ims0 <32973352+ims0@users.noreply.github.com>
2020-05-10 10:05:16 +0800

    modify build.sh

--- testFrameWork/fakeApp/project/build.sh
+++ testFrameWork/fakeApp/project/build.sh
@@ -1,21 +1,60 @@
-if test -d build
-then
-    rm -rf build/*
+cur_path=$(cd `dirname $0`; pwd)
+cd ${cur_path}
+echo `date "+%Y-%m-%d %H:%M:%S"`
+output_dir=output
+if [[ $* =~ 'opt' ]];then
+    build_type=Release
+    build_dir=build_release
 else
-    mkdir build
-fi
-if test -d output
-then
-    rm -rf output/*
-else
-    mkdir output
+    build_type=Debug
+    build_dir=build_debug
 fi
 
-cd build
-cmake ..
-make
+#######################PREPARE WORKS##################################
 
-# run
+function rm_or_mkdir()
+{
+    if [ $# -lt 1 ];then
+        echo "function need arg";exit 1;
+    fi
+    if [ ! -d $1 ]; then
+        mkdir -p $1
+        echo create:$1
+    else
+        rm -rf $1/*
+    fi
+}
+rm_or_mkdir $output_dir
+
+if [[ $* =~ 'inc' ]];then
+    echo "inc"
+    if [ ! -d $build_dir ]; then
+        mkdir -p $build_dir
+        echo create:$build_dir
+    fi
+else
+    rm_or_mkdir $build_dir
+fi
+####################### CCACHE ##################################
+export CC="ccache gcc"
+export CPP="ccache cpp"
+export CXX="ccache g++"
+export CCACHE_DIR="${cur_path}/ccache_tempfile"
+######################################################
+cd $build_dir
+cmake -DCMAKE_BUILD_TYPE=$build_type  ..
+## make
+corenum=$(cat /proc/cpuinfo | grep processor | wc -l)
+compile_start=`date +'%Y-%m-%d %H:%M:%S'`
+make -j$corenum
+compile_end=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$compile_start" +%s);
+end_seconds=$(date --date="$compile_end" +%s);
+echo "compile_time:"$((end_seconds-start_seconds))" s"
+####  run
 cd ../output
-pwd
 ./fakeApp
+
+run_end=`date +'%Y-%m-%d %H:%M:%S'`
+run_seconds=$(date --date="$run_end" +%s);
+echo "run_time:"$((run_seconds-end_seconds))" s"
