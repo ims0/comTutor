6c55516bd5cc9a1833b4104bdf5d966576e846b1
ims0 <optimizes@126.com>
2020-05-10 23:59:50 +0800

    modify interface

---
+++ testFrameWork/infra/include/pubsub.h
@@ -0,0 +1,26 @@
+#ifndef INFRA_INCLUDE_PUBSUB_H
+#define INFRA_INCLUDE_PUBSUB_H
+
+#include <netinet/in.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct
+{
+    uint32_t version;
+    uint16_t msgId;
+    uint16_t priority;
+    char *sessionType;
+    char *sessionInst;
+}PSMsg;
+
+int op_pub(PSMsg *psBrief, const char *data, const size_t dataLen);
+int  op_sub(const char* sessionType, const char* sessionInst, const size_t pno);
+
+#ifdef __cplusplus
+}
+#endif
+ 
+#endif
---
+++ testFrameWork/infra/src/pubsub.c
@@ -0,0 +1,12 @@
+#include"pubsub.h"
+
+
+int op_pub(PSMsg *psBrief, const char *data, const size_t dataLen)
+{
+    return 0;
+}
+
+int  op_sub(const char* sessionType, const char* sessionInst, const size_t pno)
+{
+    return 0;
+}
