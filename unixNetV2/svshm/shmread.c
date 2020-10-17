164a46e1b00cb1a69753f36a101e1c03ffe16e99
ims <optimizes@126.com>
2020-10-17 13:53:43 +0800

    add src systemV share mem test

--- unixNetV2/svshm/shmread.c
+++ unixNetV2/svshm/shmread.c
@@ -1,23 +1,25 @@
-#include	"unpipc.h"
+#include "unpipc.h"
 
-int
-main(int argc, char **argv)
-{
-	int		i, id;
-	struct shmid_ds	buff;
-	unsigned char	c, *ptr;
+int main(int argc, char **argv) {
+  int i, id;
+  struct shmid_ds buff;
+  unsigned char c, *ptr;
 
-	if (argc != 2)
-		err_quit("usage: shmread <pathname>");
+  if (argc != 2)
+    err_quit("usage: shmread <pathname>");
 
-	id = Shmget(Ftok(argv[1], 0), 0, SVSHM_MODE);
-	ptr = Shmat(id, NULL, 0);
-	Shmctl(id, IPC_STAT, &buff);
+  id = Shmget(Ftok(argv[1], 0), 0, SVSHM_MODE);
+  ptr = Shmat(id, NULL, 0);
+  Shmctl(id, IPC_STAT, &buff);
 
-		/* 4check that ptr[0] = 0, ptr[1] = 1, etc. */
-	for (i = 0; i < buff.shm_segsz; i++)
-		if ( (c = *ptr++) != (i % 256))
-			err_ret("ptr[%d] = %d", i, c);
+  printf("get memId:%d, length = %zu\n", id, buff.shm_segsz);
+  /* 4check that ptr[0] = 0, ptr[1] = 1, etc. */
+  
+  for (i = 0; i < buff.shm_segsz; i++)
+    if ((c = *ptr++) != (i % 256))
+      err_ret("ptr[%d] = %d", i, c);
+    else
+      puts("ok");
 
-	exit(0);
+  exit(0);
 }
---
+++ unixNetV2/svshm/srcshmtest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define SVSHM_MODE (SHM_R | SHM_W | SHM_R >> 3 | SHM_R >> 6)
+int main(int argc, char **argv) {
+  struct shmid_ds ds_buff;
+
+  if (argc != 3) {
+    puts("usage: arg: <pathname> <length>");
+    exit(1);
+  }
+  int length = atoi(argv[2]);
+  // IPC_EXCL
+  int memid = shmget(ftok(argv[1], 0), length, SVSHM_MODE | IPC_CREAT);
+  printf("get memId:%d, length = %d\n", memid, length);
+  char *ptr = (char *)shmat(memid, NULL, 0);
+  if (ptr == NULL) {
+    puts("ptr == NULL");
+    exit(1);
+  }
+
+  shmctl(memid, IPC_STAT, &ds_buff);
+
+  /* 4check that ptr[0] = 0, ptr[1] = 1, etc. */
+
+  printf("ds_buff.shm_segsz = %zu\n", ds_buff.shm_segsz);
+  for (size_t i = 0; i < ds_buff.shm_segsz; i++)
+    *ptr++ = i % 256;
+
+  sleep(10);
+  shmctl(memid, IPC_RMID, NULL); // delete
+  return 0;
+}
