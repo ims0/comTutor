17ed0d806b6ea6db93d2b75796be6557a2d6c11b
ims <optimizes@126.com>
2021-01-23 12:13:47 +0800

    1

--- item/check_zh_cn/check_str.sh
+++ item/check_zh_cn/check_str.sh
@@ -1,64 +1,74 @@
+#!/bin/bash
 ##########################################################
 # File Name: check_str.sh
 # Author: ims
 # Created Time: 2020年12月13日 星期日 13时57分39秒
 ##########################################################
-#!/bin/bash
 curr_path=$(cd `dirname $0`; pwd)
 echo $curr_path
 
-if [ $# != 2 ];then
-    echo "need path arg!"
+if [ $# != 1 ];then
+    echo "need code dir arg!"
     exit 1
 fi
 
-if [ ! -e $1 ];then
-    echo "exe not exist!"
+if [ ! -d $1 ];then
+    echo "dir not exist!"
     exit 2
 fi
-if [ ! -d $2 ];then
-    echo "dir not exist!"
-    exit 3
-fi
-exe_file=$1
-src_dir=$2
 
-logfile=log
-echo "" >$logfile
 
-function doCheck() {
-    file=$1
-    if [ "${file##*.}"x = "cpp"x ]||[ "${file##*.}"x = "c"x ]||[ "${file##*.}"x = "cc"x ]||[ "${file##*.}"x = "h"x ];then
-        file $dir_or_file |grep BOM > /dev/null
-        if [ $? == 0 ];then
-            sed  '1 s/^\xef\xbb\xbf//' $dir_or_file >nobom_tem
-            dir_or_file=nobom_tem
-        fi
-        ./$exe_file $dir_or_file>>$logfile
-        if [ $? == 1 ];then
-            file $dir_or_file |tee -a $logfile
-        fi
-    elif [ "${file##*.}"x = "lua"x ];then
-        file $dir_or_file |grep text > /dev/null
-        if [ $? == 0 ];then
-            ./$exe_file $dir_or_file>>$logfile  lua
-            if [ $? == 1 ];then
-                file $dir_or_file |tee -a $logfile
-            fi
-        fi
+exe_name=check_exe
+exe_file=$curr_path/$exe_name
+src_dir=`pwd`/$1
+logfile=$curr_path/log
+cat /dev/null >$logfile
+
+# compile file
+set -x
+gcc $curr_path/zh_str_check.c -std=c99 -O2 -o $exe_file
+set +x
+
+function callExeCheck(){
+    $exe_file $1 $2 >>$logfile
+    if [ $? != 0 ];then
+        file $1 |tee -a $logfile
     fi
 }
 
-function getdir(){
+
+function doCheck() {
+    fileName=$1
+    if [ "${fileName##*.}"x = "cpp"x ]||[ "${fileName##*.}"x = "c"x ]||[ "${fileName##*.}"x = "cc"x ]||[ "${fileName##*.}"x = "h"x ];then
+        callExeCheck $fileName cpp
+    elif [ "${fileName##*.}"x = "lua"x ];then
+        file $fileName |grep text > /dev/null
+        if [ $? == 0 ];then
+            callExeCheck $fileName lua
+        fi
+    elif [ "${fileName##*.}"x = "py"x ];then
+        callExeCheck $fileName python
+    fi
+}
+
+
+function check(){
     for element in `ls $1`
     do
         dir_or_file=$1"/"$element
         if [ -d $dir_or_file ] ;then
-            getdir $dir_or_file
+            check $dir_or_file
         else
             doCheck $dir_or_file
         fi
     done
 }
+
+$exe_file  $curr_path/test_file_dir/en.c cpp
+if [[ $? == 0 ]];then
+    echo "unit test c language en fail"
+    exit 2
+fi
+
 
 getdir $src_dir
