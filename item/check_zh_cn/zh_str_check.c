7dcf5054d45a50f97ffc65392a80375984cb1e2b
ims <optimizes@126.com>
2021-01-24 16:32:54 +0800

    finish zh_str_check.c file, test pass

--- item/check_zh_cn/zh_str_check.c
+++ item/check_zh_cn/zh_str_check.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -8,9 +8,8 @@
 #define HEX_BYTE_BB -69
 #define HEX_BYTE_BF -65
 
-enum ReturnCode
-{
-  SUCEESS=0,
+enum ReturnCode {
+  SUCEESS = 0,
   ARGC_ERR,
   FILE_NAME_ERR,
   FILE_TYPE_ERR,
@@ -23,7 +22,8 @@ void remove_python_annotation(char *buf, size_t size);
 
 int check_zh_cn(char str[], int len) {
   int i = 0;
-  if(len>3 && str[0] == HEX_BYTE_EF && str[1] == HEX_BYTE_BB &&  str[2] == HEX_BYTE_BF){
+  if (len > 3 && str[0] == HEX_BYTE_EF && str[1] == HEX_BYTE_BB &&
+      str[2] == HEX_BYTE_BF) {
     i = 3;
   }
   for (; i < len; i++) {
@@ -31,8 +31,7 @@ int check_zh_cn(char str[], int len) {
       if (len - i > 20) {
         str[i + 20] = '\0';
         printf("zh_cn str:%s\n", str + i);
-      }
-      else{
+      } else {
         printf("zh_cn str:%s\n", str + i);
       }
       return INCLUDE_ZH_ERR;
@@ -40,17 +39,18 @@ int check_zh_cn(char str[], int len) {
   }
   return 0;
 }
-int remove_anno_and_check(char *buf, int bufSize, FILE *fp, const char *filetype) {
+int remove_anno_and_check(char *buf, int bufSize, FILE *fp,
+                          const char *filetype) {
   int read_len = fread(buf, 1, bufSize, fp);
-  if(strcmp(filetype, "cpp") == 0)
+  if (strcmp(filetype, "cpp") == 0)
     remove_c_annotation(buf, read_len);
-  else if(strcmp(filetype, "lua") == 0)
+  else if (strcmp(filetype, "lua") == 0)
     remove_lua_annotation(buf, read_len);
-  else if(strcmp(filetype, "python") == 0)
+  else if (strcmp(filetype, "python") == 0)
     remove_python_annotation(buf, read_len);
   else
     return FILE_TYPE_ERR;
-  printf("-----code-----\n%s\n", buf);
+  //printf("-----code-----\n%s\n", buf);
   return check_zh_cn(buf, read_len);
 }
 
@@ -77,10 +77,10 @@ int main(int argc, char *argv[]) {
   fclose(fp);
   return result;
 }
-static int slash_is_single(const char *ptr){
+static int slash_is_single(const char *ptr) {
   const char *iter = ptr;
   int cnt = 0;
-  while(*iter == '\\'){
+  while (*iter == '\\') {
     iter--;
     cnt++;
   }
@@ -89,92 +89,85 @@ static int slash_is_single(const char *ptr){
 void remove_c_annotation(char *buf, size_t size) {
   char *p = buf, *end = buf + size, c;
   char *single_quote = 0, *double_quote = 0, *line_annotation = 0,
-     *block_annotation = 0;
+       *block_annotation = 0;
   size_t len = 0;
 
   while (p < end) {
     c = *p;
     switch (c) {
-      case '\'':
-        if (double_quote || line_annotation ||
-            block_annotation)
-        {
-          p++;
-          continue;
-        }
-        if (single_quote == NULL) 
-        {
-          single_quote = p++;
-        } else {
-          len = p++ - single_quote;
-          if (len == 2 && *(single_quote + 1) == '\\')
-          {
-            continue;
-          }
-          single_quote = NULL;
-        }
-        break;
-      case '\"':
-        if (single_quote || line_annotation || block_annotation) {
-          p++;
-          continue;
-        }
-        if (double_quote == NULL) {
-          double_quote = p++;
-        } else {
-          if (slash_is_single(p-1))
-          {
-            p++;
-            continue;
-          }
-          double_quote = NULL;
-        }
-        break;
-      case '/':
-        if (single_quote || double_quote || line_annotation || block_annotation) {
-          p++;
-          continue;
-        }
-        c = *(p + 1);
-        if (c == '/')
-        {
-          line_annotation = p;
-          p += 2;
-        } else if (c == '*') {
-          block_annotation = p;
-          p += 2;
-        } else {
-          p++;
-        }
-        break;
-      case '*':
-        if (single_quote || double_quote || line_annotation ||
-            block_annotation == NULL) {
-          p++;
-          continue;
-        }
-        if (*(p + 1) != '/')
-        {
-          p++;
-          continue;
-        }
-        p += 2;
-        memset(block_annotation, ' ', p - block_annotation);
-        block_annotation = NULL;
-        break;
-      case '\n':
-        if (line_annotation == NULL) {
-          p++;
-          continue;
-        }
-        c = *(p - 1);
-        memset(line_annotation, ' ',
-            (c == '\r' ? (p++ - 1) : p++) - line_annotation);
-        line_annotation = NULL;
-        break;
-      default:
+    case '\'':
+      if (double_quote || line_annotation || block_annotation) {
         p++;
-        break;
+        continue;
+      }
+      if (single_quote == NULL) {
+        single_quote = p++;
+      } else {
+        len = p++ - single_quote;
+        if (len == 2 && *(single_quote + 1) == '\\') {
+          continue;
+        }
+        single_quote = NULL;
+      }
+      break;
+    case '\"':
+      if (single_quote || line_annotation || block_annotation) {
+        p++;
+        continue;
+      }
+      if (double_quote == NULL) {
+        double_quote = p++;
+      } else {
+        if (slash_is_single(p - 1)) {
+          p++;
+          continue;
+        }
+        double_quote = NULL;
+      }
+      break;
+    case '/':
+      if (single_quote || double_quote || line_annotation || block_annotation) {
+        p++;
+        continue;
+      }
+      c = *(p + 1);
+      if (c == '/') {
+        line_annotation = p;
+        p += 2;
+      } else if (c == '*') {
+        block_annotation = p;
+        p += 2;
+      } else {
+        p++;
+      }
+      break;
+    case '*':
+      if (single_quote || double_quote || line_annotation ||
+          block_annotation == NULL) {
+        p++;
+        continue;
+      }
+      if (*(p + 1) != '/') {
+        p++;
+        continue;
+      }
+      p += 2;
+      memset(block_annotation, ' ', p - block_annotation);
+      block_annotation = NULL;
+      break;
+    case '\n':
+      if (line_annotation == NULL) {
+        p++;
+        continue;
+      }
+      c = *(p - 1);
+      memset(line_annotation, ' ',
+             (c == '\r' ? (p++ - 1) : p++) - line_annotation);
+      line_annotation = NULL;
+      break;
+    default:
+      p++;
+      break;
     }
   }
   if (line_annotation) {
@@ -184,90 +177,84 @@ void remove_c_annotation(char *buf, size_t size) {
 void remove_lua_annotation(char *buf, size_t size) {
   char *p = buf, *end = buf + size, c;
   char *single_quote = 0, *double_quote = 0, *line_annotation = 0,
-     *block_annotation = 0;
+       *block_annotation = 0;
   size_t len = 0;
 
   while (p < end) {
     c = *p;
     switch (c) {
-      case '\'':
-        if (double_quote || line_annotation ||
-            block_annotation)
-        {
-          p++;
-          continue;
-        }
-        if (single_quote == NULL)
-        {
-          single_quote = p++;
-        } else {
-          len = p++ - single_quote;
-          if (len == 2 && *(single_quote + 1) == '\\')
-          {
-            continue;
-          }
-          single_quote = NULL;
-        }
-        break;
-      case '\"':
-        if (single_quote || line_annotation || block_annotation) {
-          p++;
-          continue;
-        }
-        if (double_quote == NULL) {
-          double_quote = p++;
-        } else {
-          if (*(p++ - 1) == '\\') 
-          {
-            continue;
-          }
-          double_quote = NULL;
-        }
-        break;
-      case '-':
-        if (single_quote || double_quote || line_annotation || block_annotation) {
-          p++;
-          continue;
-        }
-        if (*(p + 1) == '-') {
-          if( *(p + 2) == '[' && *(p + 3) == '[' ) {
-            block_annotation = p;
-            p += 4;
-          } else{
-            line_annotation = p;
-            p = p+2;
-          }
-        } else { 
-          p++;
-        }
-        break;
-      case ']':
-        if (line_annotation ) {
-          p++;
-          continue;
-        }
-        if( block_annotation && (*(p + 1) == ']'))
-        {
-           p += 2;
-           memset(block_annotation, ' ', p - block_annotation);
-           block_annotation = NULL;
-        } else{
-            p++;
-        }
-        break;
-      case '\n':
-        if (line_annotation == NULL) {
-          p++;
-          continue;
-        }
-        c = *(p - 1);
-        memset(line_annotation, ' ',
-            (c == '\r' ? (p++ - 1) : p++) - line_annotation);
-        line_annotation = NULL;
-        break;
-      default:
+    case '\'':
+      if (double_quote || line_annotation || block_annotation) {
         p++;
-        break;
+        continue;
+      }
+      if (single_quote == NULL) {
+        single_quote = p++;
+      } else {
+        len = p++ - single_quote;
+        if (len == 2 && *(single_quote + 1) == '\\') {
+          continue;
+        }
+        single_quote = NULL;
+      }
+      break;
+    case '\"':
+      if (single_quote || line_annotation || block_annotation) {
+        p++;
+        continue;
+      }
+      if (double_quote == NULL) {
+        double_quote = p++;
+      } else {
+        if (*(p++ - 1) == '\\') {
+          continue;
+        }
+        double_quote = NULL;
+      }
+      break;
+    case '-':
+      if (single_quote || double_quote || line_annotation || block_annotation) {
+        p++;
+        continue;
+      }
+      if (*(p + 1) == '-') {
+        if (*(p + 2) == '[' && *(p + 3) == '[') {
+          block_annotation = p;
+          p += 4;
+        } else {
+          line_annotation = p;
+          p = p + 2;
+        }
+      } else {
+        p++;
+      }
+      break;
+    case ']':
+      if (line_annotation) {
+        p++;
+        continue;
+      }
+      if (block_annotation && (*(p + 1) == ']')) {
+        p += 2;
+        memset(block_annotation, ' ', p - block_annotation);
+        block_annotation = NULL;
+      } else {
+        p++;
+      }
+      break;
+    case '\n':
+      if (line_annotation == NULL) {
+        p++;
+        continue;
+      }
+      c = *(p - 1);
+      memset(line_annotation, ' ',
+             (c == '\r' ? (p++ - 1) : p++) - line_annotation);
+      line_annotation = NULL;
+      break;
+    default:
+      p++;
+      break;
     }
   }
   if (line_annotation) {
@@ -275,11 +262,104 @@ void remove_lua_annotation(char *buf, size_t size) {
   }
 }
 
-void remove_python_annotation(char *buf, size_t size){
-
-}
 int file_size(const char *filename) {
   struct stat statbuf;
   stat(filename, &statbuf);
   return statbuf.st_size;
+}
+static int isAssign(char *iter) {
+  while (isspace(*iter)) {
+    iter--;
+  }
+  return *iter == '=' ? 1 : 0;
+}
+
+void remove_python_annotation(char *buf, size_t size) {
+  char *p = buf, *end = buf + size, c;
+  char *single_quote = 0, *double_quote = 0, *line_annotation = 0,
+       *three_single_quote = 0, *three_double_quote = 0;
+
+  while (p < end) {
+    c = *p;
+    switch (c) {
+    case '\'':
+      if (line_annotation || double_quote || three_double_quote) {
+        p++;
+        continue;
+      }
+      if (*(p + 1) == '\'' && *(p + 2) == '\'') {
+        if (three_single_quote == NULL) {
+          three_single_quote = p;
+        } else {
+          p += 3;
+          if (isAssign(three_single_quote - 1)) {
+            continue;
+          } else {
+            memset(three_single_quote, ' ', p - three_single_quote);
+            three_single_quote = NULL;
+          }
+        }
+      }
+      if (single_quote == NULL) {
+        single_quote = p++;
+      } else {
+        if (*(p++ - 1) == '\\') {
+          continue;
+        }
+        single_quote = NULL;
+      }
+      break;
+    case '\"':
+      if (line_annotation || single_quote || three_single_quote) {
+        p++;
+        continue;
+      }
+      if (*(p + 1) == '\"' && *(p + 2) == '\"') {
+        if (three_double_quote == NULL) {
+          three_double_quote = p;
+        } else {
+          p += 3;
+          if (isAssign(three_double_quote - 1)) {
+            continue;
+          } else {
+            memset(three_double_quote, ' ', p - three_double_quote);
+            three_double_quote = NULL;
+          }
+        }
+      }
+      if (double_quote == NULL) {
+        double_quote = p++;
+      } else {
+        if (*(p++ - 1) == '\\') {
+          continue;
+        }
+        double_quote = NULL;
+      }
+      break;
+    case '#':
+      if (single_quote || double_quote || line_annotation ||
+          three_single_quote || three_double_quote) {
+        p++;
+        continue;
+      }
+      line_annotation = p;
+      break;
+    case '\n':
+      if (line_annotation == NULL) {
+        p++;
+        continue;
+      }
+      c = *(p - 1);
+      memset(line_annotation, ' ',
+             (c == '\r' ? (p++ - 1) : p++) - line_annotation);
+      line_annotation = NULL;
+      break;
+    default:
+      p++;
+      break;
+    }
+  }
+  if (line_annotation) {
+    memset(line_annotation, ' ', p - line_annotation);
+  }
 }
