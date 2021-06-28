6f7b911832a99b079ad5514722b319fbe4ed64ad
ims0 <optimizes@126.com>
2021-06-29 00:38:58 +0800

    add src_lock dir, and add rwlock demo

---
+++ unixipc/src_lock/rwlock_demo.c
@@ -0,0 +1,97 @@
+/*********************************************************
+    > File Name: rwlock_demo.c
+    > Author: ims
+    > Created Time: Tue 22 Jun 2021 11:56:30 PM CST
+ *********************************************************/
+
+#define _MULTI_THREADED
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void checkResults(const char*str, int rc)
+{
+    printf("%s:%d\n", str, rc);
+
+}
+pthread_rwlock_t rwlock;
+
+void *rdlockThread(void *arg) {
+  int rc;
+
+  printf("Entered thread, getting read lock\n");
+  rc = pthread_rwlock_rdlock(&rwlock);
+  checkResults("pthread_rwlock_rdlock()\n", rc);
+  printf("got the rwlock read lock\n");
+
+  sleep(5);
+
+  printf("unlock the read lock\n");
+  rc = pthread_rwlock_unlock(&rwlock);
+  checkResults("pthread_rwlock_unlock()\n", rc);
+  printf("Secondary thread unlocked\n");
+  return NULL;
+}
+
+void *wrlockThread(void *arg) {
+  int rc;
+
+  printf("--Entered thread, getting WRITE lock\n");
+  rc = pthread_rwlock_wrlock(&rwlock);
+  checkResults("pthread_rwlock_wrlock()\n", rc);
+
+  printf("Got the rwlock write lock, now unlock\n");
+  rc = pthread_rwlock_unlock(&rwlock);
+  checkResults("pthread_rwlock_unlock()\n", rc);
+  printf("Secondary thread unlocked\n");
+  return NULL;
+}
+
+int main(int argc, char **argv) {
+  int rc = 0;
+  pthread_t thread, thread1;
+
+  printf("Enter Testcase - %s\n", argv[0]);
+
+  printf("Main, initialize the read write lock\n");
+  rc = pthread_rwlock_init(&rwlock, NULL);
+  checkResults("pthread_rwlock_init()\n", rc);
+
+  printf("Main, grab a read lock\n");
+  rc = pthread_rwlock_rdlock(&rwlock);
+  checkResults("pthread_rwlock_rdlock()\n", rc);
+
+  printf("Main, grab the same read lock again\n");
+  rc = pthread_rwlock_rdlock(&rwlock);
+  checkResults("pthread_rwlock_rdlock() second\n", rc);
+
+  printf("Main, create the read lock thread\n");
+  rc = pthread_create(&thread, NULL, rdlockThread, NULL);
+  checkResults("pthread_create\n", rc);
+
+  printf("Main - unlock the first read lock\n");
+  rc = pthread_rwlock_unlock(&rwlock);
+  checkResults("pthread_rwlock_unlock()\n", rc);
+
+  printf("Main, create the write lock thread\n");
+  rc = pthread_create(&thread1, NULL, wrlockThread, NULL);
+  checkResults("pthread_create\n", rc);
+
+  sleep(5);
+  printf("Main - unlock the second read lock\n");
+  rc = pthread_rwlock_unlock(&rwlock);
+  checkResults("pthread_rwlock_unlock()\n", rc);
+
+  printf("Main, wait for the threads\n");
+  rc = pthread_join(thread, NULL);
+  checkResults("pthread_join\n", rc);
+
+  rc = pthread_join(thread1, NULL);
+  checkResults("pthread_join\n", rc);
+
+  rc = pthread_rwlock_destroy(&rwlock);
+  checkResults("pthread_rwlock_destroy()\n", rc);
+
+  printf("Main completed\n");
+  return 0;
+}
