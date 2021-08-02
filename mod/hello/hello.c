7d7cbaf8ebc6068a6da3220d57c6757957a876bb
ims0 <optimizes@126.com>
2021-08-02 11:39:41 +0800

    add seqlock api to mod/hello.c

--- mod/hello/hello.c
+++ mod/hello/hello.c
@@ -1,28 +1,54 @@
-/*********************************************************
-    > File Name: hello.c
-    > Author: ims
-    > Created Time: Fri Apr  9 19:15:35 2021
- *********************************************************/
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
 
+/*  seqlock
+ * doc: https://www.kernel.org/doc/html/latest/locking/seqlock.html
+ *  */
+#include <linux/seqlock.h>
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Hcamal");
 
-int hello_init(void)
-{
-    int size = sizeof(struct task_struct);
-    printk(KERN_INFO "Hello World\n");
-    printk(KERN_INFO "task_struct size:%d\n", size);
-    return 0;
+/* dynamic */
+seqlock_t foo_seqlock;
+
+/* static */
+static DEFINE_SEQLOCK(foo_seqlock_s);
+
+/* C99 struct init */
+/*
+struct {
+        .seql   = __SEQLOCK_UNLOCKED(foo.seql)
+} foo;
+*/
+
+void lock_init(void) {
+  /* seqlock  dynamic init */
+  seqlock_init(&foo_seqlock);
+}
+void read_seq_lock(void) {
+  unsigned seq = 0;
+  do {
+    seq = read_seqbegin(&foo_seqlock);
+    /* ... [[read-side critical section]] ... */
+  } while (read_seqretry(&foo_seqlock, seq));
+}
+void write_seq_lock(void) {
+  write_seqlock(&foo_seqlock);
+  /* ... [[write-side critical section]] ... */
+  write_sequnlock(&foo_seqlock);
+}
+int hello_init(void) {
+  int size = sizeof(struct task_struct);
+  printk(KERN_INFO "Hello World\n");
+  printk(KERN_INFO "task_struct size:%d\n", size);
+  lock_init();
+  read_seq_lock();
+  write_seq_lock();
+  return 0;
 }
 
-void hello_exit(void)
-{
-    printk(KERN_INFO "Goodbye World\n");
-}
+void hello_exit(void) { printk(KERN_INFO "Goodbye World\n"); }
 
 module_init(hello_init);
 module_exit(hello_exit);
